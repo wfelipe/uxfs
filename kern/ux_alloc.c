ca188bebf80b322aa4cc6f1c84368431526c312a
Wilson Felipe <wfelipe@gmail.com>
2009-12-22 08:17:04 -0200

    - coding style - added me as part of the author

--- kern/ux_alloc.c
+++ kern/ux_alloc.c
@@ -16,27 +16,28 @@
  * the inode number.
  */
 
-ino_t
-ux_ialloc(struct super_block *sb)
+ino_t ux_ialloc (struct super_block *sb)
 {
-        struct ux_fs          *fs = (struct ux_fs *)sb->s_private;
-        struct ux_superblock  *usb = fs->u_sb;
-        int                   i;
+	struct ux_fs *fs = (struct ux_fs *) sb->s_private;
+	struct ux_superblock *usb = fs->u_sb;
+	int	i;
 
-        if (usb->s_nifree == 0) {
-                printk("uxfs: Out of inodes\n");
-                return 0;
-        }
-        for (i = 3 ; i < UX_MAXFILES ; i++) {
-                if (usb->s_inode[i] == UX_INODE_FREE) {
-                        usb->s_inode[i] = UX_INODE_INUSE;
-                        usb->s_nifree--;
-                        sb->s_dirt = 1;
-                        return i;
-                }
-        }
-        printk("uxfs: ux_ialloc - We should never reach here\n");
-        return 0;
+	if (usb->s_nifree == 0)
+	{
+		printk("uxfs: Out of inodes\n");
+		return 0;
+	}
+	for (i = 3; i < UX_MAXFILES; i++)
+	{
+		if (usb->s_inode[i] == UX_INODE_FREE) {
+			usb->s_inode[i] = UX_INODE_INUSE;
+			usb->s_nifree--;
+			sb->s_dirt = 1;
+			return i;
+		}
+	}
+	printk ("uxfs: ux_ialloc - We should never reach here\n");
+	return 0;
 }
 
 /*
@@ -44,33 +45,35 @@ ux_ialloc(struct super_block *sb)
  * the new block  number.
  */
 
-__u32
-ux_block_alloc(struct super_block *sb)
+__u32 ux_block_alloc (struct super_block *sb)
 {
-        struct ux_fs          *fs = (struct ux_fs *)sb->s_private;
-        struct ux_superblock  *usb = fs->u_sb;
-        int                   i;
+	struct ux_fs *fs = (struct ux_fs *) sb->s_private;
+	struct ux_superblock  *usb = fs->u_sb;
+	int		   i;
 
-        if (usb->s_nbfree == 0) {
-                printk("uxfs: Out of space\n");
-                return 0;
-        }
+	if (usb->s_nbfree == 0)
+	{
+		printk("uxfs: Out of space\n");
+		return 0;
+	}
 
-        /*
-         * Start looking at block 1. Block 0 is 
-         * for the root directory.
-         */
+	/*
+	 * Start looking at block 1. Block 0 is 
+	 * for the root directory.
+	 */
 
-        for (i = 1 ; i < UX_MAXBLOCKS ; i++) {
-                if (usb->s_block[i] == UX_BLOCK_FREE) {
-                        usb->s_block[i] = UX_BLOCK_INUSE;
-                        usb->s_nbfree--;
-                        sb->s_dirt = 1;
-                        return UX_FIRST_DATA_BLOCK + i;
-                }
-        }
-        printk("uxfs: ux_block_alloc - "
-               "We should never reach here\n");
-        return 0;
+	for (i = 1 ; i < UX_MAXBLOCKS ; i++)
+	{
+		if (usb->s_block[i] == UX_BLOCK_FREE)
+		{
+			usb->s_block[i] = UX_BLOCK_INUSE;
+			usb->s_nbfree--;
+			sb->s_dirt = 1;
+			return UX_FIRST_DATA_BLOCK + i;
+		}
+	}
+	printk ("uxfs: ux_block_alloc - "
+		"We should never reach here\n");
+	return 0;
 }
 
--- kern/ux_file.c
+++ kern/ux_file.c
@@ -8,89 +8,84 @@
 #include "ux_fs.h"
 
 struct file_operations ux_file_operations = {
-        llseek:    generic_file_llseek,
-        read:      generic_file_read,
-        write:     generic_file_write,
-        mmap:      generic_file_mmap,
+	.llseek = generic_file_llseek,
+	.read = generic_file_read,
+	.write = generic_file_write,
+	.mmap = generic_file_mmap,
 };
 
-int
-ux_get_block(struct inode *inode, long block, 
-             struct buffer_head *bh_result, int create)
+int ux_get_block (struct inode *inode, long block, 
+	struct buffer_head *bh_result, int create)
 {
-        struct super_block *sb = inode->i_sb;
-        struct ux_fs       *fs = (struct ux_fs *)
-                                  sb->s_private;
-        struct ux_inode    *uip = (struct ux_inode *)
-                                   &inode->i_private;
-        __u32              blk;
+	struct super_block *sb = inode->i_sb;
+	struct ux_fs *fs = (struct ux_fs *) sb->s_private;
+	struct ux_inode *uip = (struct ux_inode *) &inode->i_private;
+   	__u32 blk;
 
-        /*
-         * First check to see is the file can be extended.
-         */
+	/*
+	 * First check to see is the file can be extended.
+	 */
 
-        if (block >= UX_DIRECT_BLOCKS) {
-                return -EFBIG;
-        }
+	if (block >= UX_DIRECT_BLOCKS)
+		return -EFBIG;
 
-        /*
-         * If we're creating, we must allocate a new block.
-         */
+	/*
+	 * If we're creating, we must allocate a new block.
+	 */
 
-        if (create) {
-                blk = ux_block_alloc(sb);
-                if (blk == 0) {
-                        printk("uxfs: ux_get_block - "
-                               "Out of space\n");
-                        return -ENOSPC;
-                }
-                uip->i_addr[block] = blk;
-                uip->i_blocks++;
-                uip->i_size = inode->i_size;
-                mark_inode_dirty(inode);
-        }
-        bh_result->b_dev = inode->i_dev;
-        bh_result->b_blocknr = uip->i_addr[block];
-        bh_result->b_state |= (1UL << BH_Mapped);
-        return 0;
+	if (create)
+	{
+		blk = ux_block_alloc (sb);
+		if (blk == 0)
+		{
+			printk("uxfs: ux_get_block - "
+				"Out of space\n");
+			return -ENOSPC;
+		}
+		uip->i_addr[block] = blk;
+		uip->i_blocks++;
+		uip->i_size = inode->i_size;
+		mark_inode_dirty (inode);
+	}
+	bh_result->b_dev = inode->i_dev;
+	bh_result->b_blocknr = uip->i_addr[block];
+	bh_result->b_state |= (1UL << BH_Mapped);
+
+	return 0;
 }
 
-int
-ux_writepage(struct page *page)
+int ux_writepage (struct page *page)
 {
-        return block_write_full_page(page, ux_get_block);
+	return block_write_full_page (page, ux_get_block);
 }
 
-int
-ux_readpage(struct file *file, struct page *page)
+int ux_readpage (struct file *file, struct page *page)
 {
-        return block_read_full_page(page, ux_get_block);
+	return block_read_full_page (page, ux_get_block);
 }
 
-int
-ux_prepare_write(struct file *file, struct page *page,
-                 unsigned from, unsigned to)
+int ux_prepare_write (struct file *file, struct page *page,
+	unsigned from, unsigned to)
 {
-        return block_prepare_write(page, from, to, ux_get_block);
+	return block_prepare_write (page, from, to, ux_get_block);
 }
 
-int
-ux_bmap(struct address_space *mapping, long block)
+int ux_bmap (struct address_space *mapping, long block)
 {
-        return generic_block_bmap(mapping, block, ux_get_block);
+	return generic_block_bmap (mapping, block, ux_get_block);
 }
 
 struct address_space_operations ux_aops = {
-        readpage:         ux_readpage,
-        writepage:        ux_writepage,
-        sync_page:        block_sync_page,
-        prepare_write:    ux_prepare_write,
-        commit_write:     generic_commit_write,
-        bmap:             ux_bmap,
+	.readpage = ux_readpage,
+	.writepage = ux_writepage,
+	.sync_page = block_sync_page,
+	.prepare_write = ux_prepare_write,
+	.commit_write = generic_commit_write,
+	.bmap = ux_bmap,
 };
 
 struct inode_operations ux_file_inops = {
-        link:             ux_link,
-        unlink:           ux_unlink,
+	.link = ux_link,
+	.unlink = ux_unlink,
 };
 
